a86f74feff390af64e87ecbdc531df2c1ec56fe0
Mariia Guk <mashahuk27@gmail.com>
2024-12-17 15:41:59 +0200

    Fixing mistakes

--- Lab4_additional21(2)/Program.cs
+++ Lab4_additional21(2)/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Lab4_additional21
 {
@@ -14,45 +15,30 @@ namespace Lab4_additional21
             int N2 = int.Parse(Console.ReadLine());
             int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            int[] Times = new int[N1 + N2];
-            int[] Skyscrapers = new int[N1 + N2];
+            string[] result = new string[N1 + N2];
 
             for (int i = 0; i < N1; i++)
             {
-                Times[i] = array1[i] + t1;
-                Skyscrapers[i] = 1;
-            }
-            for (int i = N1; i < N1+N2; i++)
-            {
-                Times[i] = array2[i-N1] + t2;
-                Skyscrapers[i] = 2;
+                result[i] = array1[i] + t1 + " 1 " + (i + 1);
             }
 
-            //Інформація https://stackoverflow.com/questions/59907372/how-to-use-this-combined-data-type-int-int
-            //У кожному кортежі перший елемент відповідає часу з масиву Times, а другий елемент – відповідному числу з масиву Skyscrapers
-            (int, int)[] pairs = new (int, int)[N1 + N2];
-            for (int i = 0; i < N1 + N2; i++)
+            for (int i = 0; i < N2; i++)
             {
-                pairs[i] = (Times[i], Skyscrapers[i]);
+                int arrivalTime = array2[i] + t2;
+                result[N1 + i] = arrivalTime + " 2 " + (i + 1);
             }
-            //Тобто, для прикладу з умови:
-            //Для i = 0: pairs[0] = (10, 1)
-            //Для i = 1: pairs[1] = (14, 1)
-            //Для i = 2: pairs[2] = (17, 1)...
-            //pairs = [(10, 1), (14, 1), (17, 1), (15, 2), (18, 2)]
 
-            //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions
-            //Cортує масив pairs за першим елементом кожного кортежу (Item1) – тобто за значеннями з масиву Times.
-            Array.Sort(pairs, (pair1, pair2) => pair1.Item1.CompareTo(pair2.Item1));
-            //pairs = [(10, 1), (14, 1), (15, 2), (17, 1), (18, 2)]
+            //OrderBy (сортування за обраним ключем) https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.orderby?view=net-9.0
+            //Інформація про лямбда-вирази https://learn.microsoft.com/uk-ua/dotnet/csharp/language-reference/operators/lambda-operator
+            //ToArray (перетворення послідовності в масив) https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.toarray?view=net-6.0
+            result = result.OrderBy(element => int.Parse(element.Split()[0])).ToArray();
+            //OrderBy(element => key), element — кожен елемент масиву; key — значення, за яким проводиться сортування (тут int.Parse(element.Split()[0]))).
+            //В результаті масив сортується за першим елементом кожного підмасиву в масиві result.
+            //Приклад: result = [[10, 1, 1], [14, 1, 2], [17, 1, 3], [15, 2, 1], [18, 2, 2]] => result = [[10, 1, 1], [14, 1, 2], [15, 2, 1], [17, 1, 3], [18, 2, 2]]
 
-            int Order1 = 0;
-            int Order2 = 0;
-
-            for (int q = 0; q < Times.Length; q++)
+            foreach (string v in result)
             {
-                if (pairs[q].Item2 == 1) Console.WriteLine($"{pairs[q].Item1} {pairs[q].Item2} {++Order1}");
-                else Console.WriteLine($"{pairs[q].Item1} {pairs[q].Item2} {++Order2}");
+                Console.WriteLine(v);
             }
         }
     }
